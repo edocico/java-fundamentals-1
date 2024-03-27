4cd39ecdccedf2851d469830b645dfd98e429341
edocico <138378767+edocico@users.noreply.github.com>
2024-03-27 15:42:03 +0100

    fix cibipreferiti

--- src/org/experis/cibi/CibiPreferiti.java
+++ src/org/experis/cibi/CibiPreferiti.java
@@ -6,12 +6,16 @@ public class CibiPreferiti {
         String[] favouriteFoods = {"Pizza", "Carbonara", "Piadina", "Risotto", "Pesce", };
         // stampo lunghezza classifica
         System.out.println("La classifica è lunga:" + " " + favouriteFoods.length);
+        // variabili
+        int topPos = 0;
+        int middlePosition = (favouriteFoods.length - 1) / 2;
+        int bottomPosition = favouriteFoods.length -1;
         // stampo posizione top
-        System.out.println("Cibo top è:" + " " + favouriteFoods[0]);
+        System.out.println("Cibo top è:" + " " + favouriteFoods[topPos]);
         // stampo posizione bottom
-        System.out.println("Cibo non troppo preferito è:" + " " + favouriteFoods[4]);
+        System.out.println("Cibo non troppo preferito è:" + " " + favouriteFoods[middlePosition]);
         // stampo posizione mediana
-        System.out.println("Cibo in posizione mediana è:" + " " + favouriteFoods[2]);
+        System.out.println("Cibo in posizione mediana è:" + " " + favouriteFoods[bottomPosition]);
 
     }
 }
