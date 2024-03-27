4437b9d4b8eb668d0a7eeaf14b3a233f47a46ebe
edocico <138378767+edocico@users.noreply.github.com>
2024-03-27 14:38:47 +0100

    Calcolo biglietto

---
+++ src/org/experis/treno/BigliettoTreno.java
@@ -0,0 +1,42 @@
+package org.experis.treno;
+
+import java.util.Scanner;
+
+public class BigliettoTreno {
+    public static void main(String[] args) {
+        // definisco variabili conosciute e non
+        double priceXKm = 0.21;
+        int minUserAge = 18;
+        int maxUserAge = 65;
+        double desireKm;
+        int userAge;
+        int juniorDisc = 20;
+        int seniorDisc = 40;
+
+        // scanner
+
+        Scanner scan = new Scanner(System.in);
+        // chiedo quanti km
+        System.out.print("Quanti Km vuoi percorrere ?");
+        // salvo valore in variabile
+        desireKm = scan.nextInt();
+        // chiedo età passeggero
+        System.out.print("Quanti anni hai ?");
+        userAge = scan.nextInt();
+        // calcolo il prezzo del biglietto senza sconto
+        double PriceWoDisc = priceXKm * desireKm;
+        // dichiaro variabile prezzo finale
+        double endPrice;
+        // calcolo il prezzo in base alla casistica di sconto
+
+        if(userAge < minUserAge) {
+            endPrice = (PriceWoDisc * 100) / juniorDisc;
+        } else if (userAge > maxUserAge) {
+            endPrice = (PriceWoDisc * 100) / seniorDisc;
+        } else {
+            endPrice = PriceWoDisc;
+        }
+
+        System.out.println("Il prezzo del tuo biglietto è:" + " " + endPrice);
+    }
+}
