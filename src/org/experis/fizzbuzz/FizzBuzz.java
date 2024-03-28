7394b15e070f5bb20e3a5bff1df9a4bfc7321431
edocico <138378767+edocico@users.noreply.github.com>
2024-03-28 10:09:58 +0100

    add comments for flexible code

--- src/org/experis/fizzbuzz/FizzBuzz.java
+++ src/org/experis/fizzbuzz/FizzBuzz.java
@@ -4,6 +4,13 @@ public class FizzBuzz {
     public static void main(String[] args) {
         // faccio ciclio da 100 iterazioni
 
+        // can be more flexible ex:
+
+        int minValue;
+        int maxValue;
+
+        // poi posso chiedere all'utente i valori da inserire
+
         for (int i = 1; i <= 100; i++) {
             if((i % 3 == 0) && (i % 5 == 0)) { // || (i % 15 == 0)
                 System.out.println(i + "fizzbuzz");
